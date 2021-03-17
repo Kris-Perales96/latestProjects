7d9a0a917ff0ed57273ba274b27d7e775e069afd
K.Perales205 <kris.perales96@outlook.com>
2021-03-17 01:12:47 -0500

    Added retrieve and send of a random message, need to change my messenger object in the terminal object

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,42 +1,103 @@
 //This will be for mixed messages project
-const messageTerminal = {
-    "Messengers" : [],
-    handleMessage(typeOfMessage, messenger, receipient, message){
-        if(!this["Messengers"].includes(recipient)){
-            this.Messengers[messenger].sendMessage('Message Handler', "That is not among the recipients, here's the current recipients.");
-            this.Messengers.forEach(element => console.log(element));
-        }
-        else{
-            switch(typeOfMessage){
-                case 'send':
-                    this["Messengers"][recipient].sendMessage(messenger, message);
-                    break;
-                case 'all':
-                    for(let index = 0; index < this["Messengers"].length; index++){
-                        this["Messengers"][index].sendMessage(messenger,message);
-                    }
-                    break;
-                default:
-                    this["Messengers"][messenger].sendMessage('Message Handler', "That is an invalid request, try all or send.");
-                    break;
-            }
-        }
-    },
-    "Random Messages" : ['Once I saw a dead armoredillow when going to school', 'Texas is poop for a severe snow storm.'],
-    sendRandomMessage(message){
-        this.Messengers.forEach(element => element.sendMessage('Message Handler', message));
-    }
-};
 
 const messengerFactory = (number, name) => {
     return{
         number,
         name,
-        sendMessge(messenger, message){
-            this.receiveMessage(messenger, message);
+        sendMessge(messenger, recipient, message){
+            this.receiveMessage(messenger,recipient, message);
         },
-        receiveMessage(messenger, message){
-            console.log(`${messenger}: ${message}`);
+        receiveMessage(messenger, recipient, message){
+            console.log(`${messenger}: sent ${recipient} ${message}`);
         },
     }
 };
+
+const messageTerminal = {
+    "Messengers" : [],
+    handleMessage(typeOfMessage, messenger, recipient, message){
+        let check = false;
+                for (let messenger in this.Messengers){
+                    if(this.Messengers[messenger].name === recipient || this.Messengers[messenger].number === recipient){
+                        check = true;
+                    }
+                }
+        if (check === false){
+            console.log('Message Terminal: ' + 'That is not among the recipients, here they are.');
+            this.printCurrentRecipients();
+        }
+        else{
+            switch(typeOfMessage){
+                case 'send':
+                    this.Messengers.sendMessage(messenger, recipient, message);
+                    break;
+                case 'all':
+                        this.Messengers.sendMessage(messenger, index, message);
+                    break;
+                default:
+                    console.log('Message Handler', "That is an invalid request, try all or send.");
+                    break;
+            }
+        }
+    },
+    "Random Messages" : ['Once I saw a dead armoredillow when going to school', 'Texas is poop for a severe snow storm.', 'Sometimes I feel like life is a third-person simulator'],
+    sendRandomMessage(message){
+        this['Messengers'].forEach(element => element.sendMessage('Message Handler', message));
+    },
+    retrieveRandomMessage(index){
+        return this["Random Messages"][index% this["Random Messages"].length] ;
+    },
+    printCurrentRecipients(){
+        this.Messengers.forEach(element => console.log(element.name, element.number));
+    },
+    runMessenger(){
+
+        this.printCurrentRecipients();
+        while(true){
+            let ranNumber = Math.floor(Math.random()* 10200);
+            let message = this.retrieveRandomMessage(ranNumber);
+            let recipient = this.Messengers[Math.floor(Math.random() * 10 % this.Messengers.length)].name;
+            console.log(recipient)
+            this.setMessageAndRecipient(message, recipient);
+            if(ranNumber % 100 === 0)
+                break;
+            if(ranNumber % 10 === 0)
+                {
+                    this.handleMessage('all', 'Message Handler', recipient, message);
+                }
+            else {
+                let check = false;
+                for (let messenger in this.Messengers){
+                    if(messenger.name === recipient || messenger.number === recipient){
+                        check = true;
+                    }
+                }
+                if(check && ranNumber%5 != 0)
+                    this.handleMessage('send', this.Messengers[Math.floor(Math.random()) * (this.Messengers.length - 1)].name, recipient, message);
+                else{
+                    this.handleMessage('na', this.Messengers[Math.floor(Math.random()) * (this.Messengers.length - 1)].name, recipient, message);
+                }
+            }
+        }
+    },
+    initialize(Messengers){
+        this.Messengers = Messengers
+        this.runMessenger();
+    },
+    setMessageAndRecipient(message, recipient){
+        this.message = message;
+        this.recipient = recipient;
+    }
+};
+
+
+let names = ['Steve', 'Mike', 'Trey', 'all']
+let numberOfUsers = 3;
+let name = '';
+let Messengers = [{}];
+while(numberOfUsers> 0 )
+{
+    Messengers.push(messengerFactory(Math.floor(Math.random() * 1000), names[numberOfUsers]));
+    numberOfUsers-=1;
+}
+messageTerminal.initialize(Messengers);
